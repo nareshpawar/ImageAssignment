b342f1a3c8d1a21416a383c6e8dec7724c0daf4b
naresh pawar <naresh.pawar@nucleonai.co.in>
2021-12-28 20:39:03 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ImageTask';
+
+  
+  firstRowFirstImage =false;
+  firstRowSecondImage =false;
+  firstRowThirdImage =false;
+  secondRowFirstImage =false;
+  secondRowsecondImage =false;
+  secondRowThirdImage =false;
+  // public dish1 ="src\assets\images\download (1).jpg"
+  addImage(value:any){
+    console.log(value);
+    if(value == 1){
+      this.firstRowFirstImage =!this.firstRowFirstImage;
+      this.firstRowSecondImage = false;
+      this.firstRowThirdImage = false;
+    }else if( value == 2){
+      this.firstRowFirstImage = false;
+      this.firstRowSecondImage =!this.firstRowSecondImage;
+      this.firstRowThirdImage = false;
+    }else if( value == 3){
+      this.firstRowFirstImage = false;
+      this.firstRowSecondImage = false;
+      this.firstRowThirdImage =!this.firstRowThirdImage;
+    }else if( value == 4){
+      this.secondRowFirstImage =!this.secondRowFirstImage;
+      this.secondRowsecondImage =false;
+      this.secondRowThirdImage =false;
+    }else if( value == 5){
+      this.secondRowFirstImage =false;
+      this.secondRowsecondImage =!this.secondRowsecondImage;
+      this.secondRowThirdImage =false;
+    }
+    else if( value == 6){
+      this.secondRowFirstImage =false;
+      this.secondRowsecondImage =false;
+      this.secondRowThirdImage =!this.secondRowThirdImage;
+    }
+
+  }
 }
+
